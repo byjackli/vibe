5f33e265a5b38b87a3875c4de83f6e65375ae1a2
Jack Li <33737148+byjackli@users.noreply.github.com>
2022-12-29 22:25:02 -0500

    add CSS buttons + tests --- - added static font styles for local hosting - added static icon font styles for local hosting - added fonts css to handle static fonts - added global css to standardize basic components - added buttons css to house button-related css - update  pre-prod and prod CI/CD scripts - upgrade playwright to use .all() property - add firebase hosting - create sample buttons for testing

---
+++ src/routes/styleguide/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Styleguide - ViBE</title>
+</svelte:head>
+
+<h1>Styleguide</h1>
+
+<section>
+	<h2>Buttons</h2>
+	<ul>
+		<li><button class="main">main button</button></li>
+		<li><button class="main invert">invert main</button></li>
+		<li><button>supporting button</button></li>
+		<li><button><span class="material-icons">support_agent</span>icon supporting</button></li>
+		<li><button class="link">link button</button></li>
+	</ul>
+</section>
