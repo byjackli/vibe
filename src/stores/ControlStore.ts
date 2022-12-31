bb95bc01ee78d5212227b314c2c8ffe76e19606e
us1aaron <78885486+us1aaron@users.noreply.github.com>
2022-12-31 13:57:00 -0500

    ControlStore

---
+++ src/stores/ControlStore.ts
@@ -0,0 +1,10 @@
+import { writable } from "svelte/store";
+let currentSongId = undefined
+
+export const ControlStore = writable({currentSongId})
+
+export function updateCurrentSong(newSongId:string):void{
+    currentSongId = newSongId
+}
+
+export default ControlStore
