0c35e18f1e46963ad9fa4d5c6484afcfff65ef44
Jack Li <33737148+byjackli@users.noreply.github.com>
2023-01-02 18:27:22 -0500

    Implemented welcome page view
    ## What
    - Implemented welcome page view
    - Added font weight 200
    - Removed font weight 100
    
    ## Why
    - font weight 100 is too light, raises accessibility concerns
    
    ## Impact
    - Will customer experience be significantly altered? No
    - Does the code change cover more than one task? No
    
    ## Testing
    - wrote tests and ran locally (passing)

--- tests/pages.test.ts
+++ tests/pages.test.ts
@@ -1,9 +1,18 @@
 import { test, expect } from "@playwright/test"
 
-test.describe("Pages", () => {
-    test(`Styleguide`, async ({ page }) => {
+test.describe("Styleguide", () => {
+    test(`setup`, async ({ page }) => {
         await page.goto('/styleguide');
-        expect(page).toHaveTitle(`Styleguide - ViBE`)
-        expect(await page.textContent('h1')).toBe(`Styleguide`)
+        await expect(page).toHaveTitle(`Styleguide - ViBE`);
+        expect(page.locator(`h1`, { hasText: "Styleguide" })).toBeTruthy()
+    })
+})
+
+test.describe("Welcome", () => {
+    test(`setup`, async ({ page }) => {
+        await page.goto('/');
+        await expect(page).toHaveTitle(`ViBE`);
+        expect(page.locator(`h1`, { hasText: "ViBE" })).toBeTruthy()
+        expect(page.locator(`button`, { hasText: "sign in with spotify" })).toBeTruthy()
     })
 })
