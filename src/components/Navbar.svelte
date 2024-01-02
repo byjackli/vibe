74c1803c34d87c4137a9e01ad573086508e3bb79
Jack Li <33737148+byjackli@users.noreply.github.com>
2024-01-01 20:17:47 -0500

    Added navbar, default page layout, and buttons
    ## What
    - Added pages for account, messages, and settings
    - Fixed font-weight font-style names
    - Added commented out no-console eslintrc rule
    - Added default layout
    - Updated styleguide page layout
    - Added navbar
    - Updated link style
    - Renamed previous `link button` style to `link underline`
    
    ## Why
    - Basic pages for project
    - CSS using font-weight set via font-style did not correctly use corresponding font weight
    - Preparing to turn off no-console unless type warn and error
    - Enable easy use of navbar and main content
    - Allow customers to easily navigate between pages
    - To align with figma design of navbar
    
    ## Impact
    - Will customer experience be significantly altered? No
    - Does the code change cover more than one task? Yes
    
    ## Testing
    - npm run lint:fix

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const denylist: string[] = [];
+	const pages: string[] = ['account', 'messages', 'settings', 'styleguide'];
+
+	$: navbarVisibility = !denylist.includes($page.url.pathname);
+	$: currentPage = $page.url.pathname.slice(1);
+</script>
+
+{#if navbarVisibility}
+	<nav aria-label="Vibe">
+		<a href="/"><span class="logo">ViBE</span></a>
+
+		<ul role="menubar" aria-label="Vibe">
+			{#each pages as page}
+				<li class={page === currentPage ? 'active' : ''}>
+					<a href="/{page}">{page}</a>
+					<span aria-hidden class="format-only">{page}</span>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+{/if}
+
+<style>
+	nav,
+	ul {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	nav {
+		width: 100%;
+		height: var(--h-navmain);
+		padding: var(--gap-m);
+		justify-content: flex-start;
+	}
+
+	ul {
+		gap: var(--gap-m);
+		list-style-type: none;
+	}
+
+	ul .active a {
+		font-weight: bold;
+		text-align: center;
+	}
+	ul a:first-of-type {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	.format-only {
+		font-weight: bold;
+		text-align: center;
+
+		opacity: 0;
+		z-index: -1;
+	}
+
+	nav .logo {
+		font-size: 1.5rem;
+	}
+</style>
---
+++ src/views/Default.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<div id="pg-layout-default">
+	<div class="navbar-container">
+		<Navbar />
+	</div>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	#pg-layout-default {
+		width: clamp(100vw, 100vw, 100vw);
+		height: clamp(100vh, 100vh, 100vh);
+
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: var(--h-navmain) 1fr;
+
+		overflow: hidden;
+	}
+	.navbar-container {
+		position: sticky;
+		top: 0;
+	}
+	main {
+		min-width: 100%;
+		min-height: 100%;
+		gap: var(--gap-xl);
+		overflow-y: auto;
+	}
+</style>
